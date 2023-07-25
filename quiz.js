1191b3fc6859505e29fdba3d61c53e90b780a866
npmnpm1111 <131908995+npmnpm1111@users.noreply.github.com>
2023-07-25 17:10:11 +0700

    still backspace

--- quiz.js
+++ quiz.js
@@ -73,7 +73,8 @@
                     answerInput.checked = true;
                 }
                 var answerLabelPrefix = document.createElement("label");
-                answerLabelPrefix.textContent = String.fromCharCode(65 + j) + ".&nbsp;"; // Thêm dấu cách sau dấu chấm
+                answerLabel.className = "answer-prefix";
+                answerLabelPrefix.textContent = String.fromCharCode(65 + j) + "."; // Thêm dấu cách sau dấu chấm
                 answerLabelPrefix.className = "answer-prefix";
                 var answerLabel = document.createElement("label");
                 answerLabel.textContent = answer;
@@ -93,7 +94,8 @@
                     answerInput.checked = true;
                 }
                 var answerLabelPrefix = document.createElement("label");
-                answerLabelPrefix.textContent = String.fromCharCode(65 + j) + ".&nbsp;"; // Thêm dấu cách sau dấu chấm
+                answerLabel.className = "answer-prefix";
+                answerLabelPrefix.textContent = String.fromCharCode(65 + j) + "."; // Thêm dấu cách sau dấu chấm
                 answerLabelPrefix.className = "answer-prefix";
                 var answerLabel = document.createElement("label");
                 answerLabel.textContent = answer;
