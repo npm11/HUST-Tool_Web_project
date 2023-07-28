2c74aa3e850ce12d3bce789f853323bf8c1242db
npmnpm1111 <131908995+npmnpm1111@users.noreply.github.com>
2023-07-28 22:43:26 +0700

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development', // hoặc 'production' tùy thuộc vào môi trường của bạn
   entry: './quiz.js',
   output: {
     filename: 'main.js',
