9b3456d9796d3bbebec9ac71fa3e02ae5a9db2c6
npmnpm1111 <131908995+npmnpm1111@users.noreply.github.com>
2024-01-23 03:26:11 +0700

    Revert

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development", // hoặc 'production' tùy thuộc vào môi trường của bạn
-  entry: "./src/js/quiz.js",
+  entry: "./quiz.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
