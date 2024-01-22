5d45f8b6771a6b8f7f1fca0d04a05383af67dfd0
npmnpm1111 <131908995+npmnpm1111@users.noreply.github.com>
2024-01-23 03:21:33 +0700

    update

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -38,7 +38,7 @@
   }
 
   // Descriptions of all options available on the demo site:
-  // http://lokeshdhakar.com/projects/lightbox2/src/html/index.html#options
+  // http://lokeshdhakar.com/projects/lightbox2/index.html#options
   Lightbox.defaults = {
     albumLabel: "Image %1 of %2",
     alwaysShowNavOnTouchDevices: false,
